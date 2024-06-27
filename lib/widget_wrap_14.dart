a468902bd44180fbd0c4a5cea45f1481825201c7
paijo-multiverse <squadpaijoganteng@gmail.com>
2024-06-27 15:52:55 +0800

    penambahan sintax pada widget wrap

--- lib/widget_wrap_14.dart
+++ lib/widget_wrap_14.dart
@@ -1,16 +1,20 @@
 import 'package:flutter/material.dart';
 
-class BelajarWrapWidget extends StatelessWidget {
-  const BelajarWrapWidget({super.key});
+class BelajarWidgetWrap extends StatelessWidget {
+  const BelajarWidgetWrap({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.amber,
-      ),
       body: Wrap(
-        
+        children: List.generate(
+          10,
+          (indexnyaDimulaiDariNol) => Container(
+            width: 100,
+            height: 100,
+            color: indexnyaDimulaiDariNol % 2 == 0 ? Colors.amber : Colors.blue,
+          ),
+        ),
       ),
     );
   }
