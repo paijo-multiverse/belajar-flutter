e11a492077e97ca7521f83971193ab78cecc3cb2
paijo-multiverse <squadpaijoganteng@gmail.com>
2024-06-29 10:36:41 +0800

    selesai belajar tentang alignment pada sebuah elemento

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:belajar/widget_wrap_14.dart';
+import 'package:belajar/text_alignment.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BelajarWidgetWrap(),
+      home: BelajarTextAlignment(),
     );
   }
 }
---
+++ lib/text_alignment.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+class BelajarTextAlignment extends StatelessWidget {
+  const BelajarTextAlignment({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Belajar ajah'),
+        backgroundColor: Colors.brown,
+        actions: [
+          IconButton(onPressed: () {}, icon: const Icon(Icons.notification_add))
+        ],
+      ),
+      body: Align(
+        // untuk mengatur posisi
+        // alignment: Alignment.topLeft,
+        // alignment: Alignment.topRight,
+        // alignment: Alignment.topCenter,
+        // alignment: Alignment.center,
+        // alignment: Alignment.bottomLeft,
+        // alignment: Alignment.bottomRight,
+        // alignment: Alignment.bottomCenter,
+
+        // atur ukuran alignment sendiri
+        alignment: Alignment(-0.5,-1),
+
+        child: SizedBox(
+          width: 300,
+          child: Text(
+            'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Fugiat sapiente odio repellendus, aspernatur repellat iste reiciendis recusandae a, laboriosam suscipit quidem accusantium id eaque officiis ipsa sed natus obcaecati exercitationem.',
+            style: TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+            textAlign: TextAlign.justify,
+          ),
+        ),
+      ),
+    );
+  }
+}
