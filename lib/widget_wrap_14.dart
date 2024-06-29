33e3db97649a43bb88a00e4c8aa4f94bf5e8af10
paijo-multiverse <squadpaijoganteng@gmail.com>
2024-06-29 09:43:26 +0800

    selesai belajar wrap widget

--- lib/widget_wrap_14.dart
+++ lib/widget_wrap_14.dart
@@ -6,13 +6,53 @@ class BelajarWidgetWrap extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Wrap(
-        children: List.generate(
-          10,
-          (indexnyaDimulaiDariNol) => Container(
-            width: 100,
-            height: 100,
-            color: indexnyaDimulaiDariNol % 2 == 0 ? Colors.amber : Colors.blue,
+      backgroundColor: Colors.amber,
+      body: Container(
+        color: Colors.yellow,
+        width: double.infinity,
+        height: double.infinity,
+        child: Wrap(
+          // direction: Axis.horizontal,
+          // direction: Axis.vertical,
+          // spacing: 10,
+          runSpacing: 5,
+          // alignment: WrapAlignment.start, default
+          // alignment: WrapAlignment.end,
+          alignment: WrapAlignment.center,
+          // alignment: WrapAlignment.spaceBetween,
+          // alignment: WrapAlignment.spaceEvenly,
+          // alignment: WrapAlignment.spaceAround,
+
+          // runAlignment: WrapAlignment.start,
+          // runAlignment: WrapAlignment.end,
+          runAlignment: WrapAlignment.center,
+          // runAlignment: WrapAlignment.spaceAround,
+          // runAlignment: WrapAlignment.end,
+          // runAlignment: WrapAlignment.end,
+
+          // untuk mengatur element didalam container
+          // crossAxisAlignment: WrapCrossAlignment.center,
+          crossAxisAlignment: WrapCrossAlignment.end,
+
+          // mengubah arah start wrap
+          // verticalDirection: VerticalDirection.down,
+          // verticalDirection: VerticalDirection.up,
+
+          children: List.generate(
+            8,
+            (paijo) => Container(
+              width: paijo % 2 == 0 ? 110 : 50,
+              height: paijo % 2 == 0 ? 110 : 50,
+              color: paijo % 2 == 0 ? Colors.red : Colors.blue,
+              child: Center(
+                  child: Text(
+                '$paijo',
+                style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              )),
+            ),
           ),
         ),
       ),
