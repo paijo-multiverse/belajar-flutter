249e6b40225aa8a8c1de1c8e5f404b2ea2dcbf36
paijo-multiverse <squadpaijoganteng@gmail.com>
2024-07-03 14:03:55 +0800

    penambahan file icon.dart

---
+++ lib/icon.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BelajarIcon extends StatelessWidget {
+  const BelajarIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:belajar/padding_widget.dart';
+import 'package:belajar/icon.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(const MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: BelajarPadding(),
+      home: BelajarIcon(),
     );
   }
 }
